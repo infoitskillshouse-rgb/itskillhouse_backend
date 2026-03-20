1c138b03cee819346cc5514fa0df02ce50327cf9
infoitskillshouse-rgb <infoitskillshouse@gmail.com>
2026-03-20 12:31:13 +0530

    update

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -22,7 +22,7 @@ router.get("/id/:id", getBlogById);     // Get blog by MongoDB ID
 router.get("/:slug", getBlogBySlug);    // Get blog by slug
 
 // ----------------- Protected Routes (Admin) -----------------
-router.post("/", isAdminAuthenticated, uploadBlog.single("image"), createBlog);
+router.post("/create", isAdminAuthenticated, uploadBlog.single("image"), createBlog);
 router.put("/:id", isAdminAuthenticated, uploadBlog.single("image"), updateBlog);
 router.delete("/:id", isAdminAuthenticated, deleteBlog);
 
